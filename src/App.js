d8f282fe72a16fa01e6eb4ac3ab56c904a1c7a8e
Karumaidoi <mainahmwangi12@gmail.com>
2023-07-12 22:04:55 +0300

    State management using reducer and hosting a local server

--- src/App.js
+++ src/App.js
@@ -1,9 +1,44 @@
-import DateCounter from "./DateCounter";
+import { useEffect, useReducer } from "react";
+import Header from "./Header";
+import Main from "./Main";
+import Loader from "./Loader";
+import Error from "./Error";
+
+const initialData = {
+  questions: [],
+  status: "loading",
+};
+
+function reducer(state, action) {
+  console.log(state, action);
+  switch (action.type) {
+    case "dataReceived":
+      return { ...state, questions: action.payload, status: "ready" };
+    case "dataFailed":
+      return { ...state, status: "error" };
+    default:
+      throw new Error("Something went wrong");
+  }
+}
 
 export default function App() {
+  const [{ questions, status }, dispatch] = useReducer(reducer, initialData);
+  console.log(status);
+
+  useEffect(function () {
+    fetch("http://localhost:9000/questions")
+      .then((res) => res.json())
+      .then((data) => dispatch({ type: "dataReceived", payload: data }))
+      .catch((err) => dispatch({ type: "dataFailed" }));
+  }, []);
+
   return (
-    <div>
-      <DateCounter />
+    <div className="app">
+      <Header />
+      <Main>
+        {status === "loading" && <Loader />}
+        {status === "error" && <Error />}
+      </Main>
     </div>
   );
 }
